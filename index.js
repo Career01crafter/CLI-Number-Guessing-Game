09c8d60394fdd87789100efa6da29ca20f5238e5
Career01Crafter <163905451+Career01crafter@users.noreply.github.com>
2024-04-15 00:25:35 +0500

    Created CLI Guessing Game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "NumberGuessedByUser",
+        type: "number",
+        message: "Please guess a number between 0 to 5 to win.",
+    },
+]);
+if (answer.NumberGuessedByUser === randomNumber) {
+    console.log("Congratulations! You guessed a right number.");
+}
+else {
+    console.log("Please try again! You guessed a wrong number.");
+}
